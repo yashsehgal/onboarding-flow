dce4fc0acafd0120306672de6b18871869c6f31b
Yash Sehgal <yashsehgal.work@gmail.com>
2022-05-19 17:34:53 +0000

    perf: state logics improved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Onboarding from "./Onboarding";
 
+import 'animate.css';
+
 export default function App() {
   const [userStatusRef] = useState(true);
 
