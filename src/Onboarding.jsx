d4b0e041acd9469fc5b49c4c094847ff8f73b479
Yash Sehgal <yashsehgal.work@gmail.com>
2022-06-09 19:54:25 +0000

    feat: progress bar animation added

--- src/Onboarding.jsx
+++ src/Onboarding.jsx
@@ -9,6 +9,8 @@ export default function Onboarding() {
     const [currentOnboardingViewRef, setCurrentOnboardingView] = useState(0);
     const [contentForOnboardingRef, setContentForOnboarding] = useState(manageOnboardingContent_onSlide());
 
+    const [onboardingProgressBarWidthRef, setOnboardingProgressBarWidth] = useState('0%');
+
     // button visibilty states
     const [previousButtonVisibilityRef, setPreviousButtonVisibility] = useState("none");
     const [nextButtonContentRef, setNextButtonContent] = useState("next");
@@ -18,6 +20,19 @@ export default function Onboarding() {
         // fetching content to show in the modal
         setContentForOnboarding(manageOnboardingContent_onSlide(currentOnboardingViewRef));
         
+        // changing the width of the progress bar according to the 
+        // slide count (by defining a percentage value with the 
+        // help of contentForOnboardingRef.totalSlideCount)
+        setOnboardingProgressBarWidth(
+            currentOnboardingViewRef ? `${contentForOnboardingRef.totalSlideCount*(currentOnboardingViewRef/100)*1000}%` : `0%`
+        )
+
+        if (currentOnboardingViewRef === contentForOnboardingRef.totalSlideCount) {
+            setOnboardingProgressBarWidth('100%');
+        }
+
+        console.log(onboardingProgressBarWidthRef);
+
         // managing visibility states for buttons
         
         // previous button will be set as none(in terms of display) 
@@ -27,7 +42,7 @@ export default function Onboarding() {
         // next button text content will be getting changed
         // once the user is on the ending slide (lastIndex slide)
         currentOnboardingViewRef === contentForOnboardingRef.totalSlideCount ? setNextButtonContent("get started 🚀") : setNextButtonContent("next");
-    }, [currentOnboardingViewRef, contentForOnboardingRef.totalSlideCount]);
+    }, [currentOnboardingViewRef, contentForOnboardingRef.totalSlideCount, onboardingProgressBarWidthRef]);
 
     return (
         <React.Fragment>
@@ -58,77 +73,98 @@ export default function Onboarding() {
                         marginLeft: 'auto',
                         marginBottom: 'auto',
                         marginTop: 'auto',
+                        // removing default padding from modal
+                        padding: '0px'
                     }
                 }}
             >
-                <div 
-                    className="text-white text-lg font-medium" 
-                    style={{
-                        width: '720px',
-                        height: 'fit-content'
-                    }}
-                >
-                    <span className="new-feature-tag-slot w-fit h-fit">
-                        {contentForOnboardingRef.content.newFeatureTag ? <p className="w-fit h-fit text-xs font-bold text-purple-500">NEW FEATURE</p> : <React.Fragment></React.Fragment>}
-                    </span>
-                    <h1 className="leading-snug text-3xl font-medium text-white mt-2">{contentForOnboardingRef.content.featureTitle}</h1>
-                    <p className="leading-snug text-white text-opacity-50 text-sm font-normal">{contentForOnboardingRef.content.featureDescription}</p>
-                    
-                    <div 
-                        className="onboarding-feature-screenshots-wrapper 
-                            mt-3 w-auto py-3 h-fit max-h-[400px] overflow-hidden
-                            flex flex-row items-center justify-start gap-2
-                        "
-                    >
-                        <img 
-                            src={contentForOnboardingRef.content.featureScreenshot}
-                            alt={contentForOnboardingRef.content.featureTitle.toString().toLowerCase()}
-                            id={contentForOnboardingRef.content.featureTitle.toString().toLowerCase()}
-                            className="mx-auto"
-                        />
-                    </div>
-                    <div className="navigation-component-wrapper">
-                        <ShowSlideNavigation 
-                            totalSlideCount={contentForOnboardingRef.totalSlideCount} 
-                            activeSlideIndex={currentOnboardingViewRef+1}    
-                        />
-                    </div>
+                {/* start: progress bar animation section wrapper */}
+                <div className="progress-bar-animation-section-wrapper h-fit w-full bg-white">
+                    <div className="p-1 bg-purple-500" 
+                        style={{
+                            width: onboardingProgressBarWidthRef
+                        }}
+                    />
                 </div>
-                <div className="button-slots-wrapper mt-4 flex flex-row items-center justify-between">
-                    <div className="left-button-slot w-fit h-fit flex flex-row items-center justify-center">
-                        <button
-                            className="text-purple-500 font-medium px-3 py-1.5 rounded-sm bg-purple-700 bg-opacity-40 hover:bg-opacity-50 uppercase bg-cover" 
-                            onClick={() => setOnboarding(false)}
+                {/* end: progress bar animation section wrapper */}
+
+                <div className="onboarding-popup-internal-content-wrapper p-6">
+                    <div 
+                        className="text-white text-lg font-medium" 
+                        style={{
+                            width: '720px',
+                            height: 'fit-content'
+                        }}
+                    >
+                        <span className="new-feature-tag-slot w-fit h-fit">
+                            {contentForOnboardingRef.content.newFeatureTag ? <p className="w-fit h-fit text-xs font-bold text-purple-500">NEW FEATURE</p> : <React.Fragment></React.Fragment>}
+                        </span>
+                        <h1 className="leading-snug text-3xl font-medium text-white mt-2">{contentForOnboardingRef.content.featureTitle}</h1>
+                        <p className="leading-snug text-white text-opacity-50 text-sm font-normal">{contentForOnboardingRef.content.featureDescription}</p>
+                        
+                        <div 
+                            className="onboarding-feature-screenshots-wrapper 
+                                mt-3 w-auto py-3 h-fit max-h-[400px] overflow-hidden
+                                flex flex-row items-center justify-start gap-2
+                            "
                         >
-                            Skip
-                        </button>
+                            <img 
+                                src={contentForOnboardingRef.content.featureScreenshot}
+                                alt={contentForOnboardingRef.content.featureTitle.toString().toLowerCase()}
+                                id={contentForOnboardingRef.content.featureTitle.toString().toLowerCase()}
+                                className="mx-auto"
+                            />
+                        </div>
+                        <div className="navigation-component-wrapper">
+                            {/* 
+                                Legacy implementation for showing navigation slider,
+                                currently, it's not working but saving it for future references
+                            */}
+
+                            {/* 
+                                <ShowSlideNavigation 
+                                    totalSlideCount={contentForOnboardingRef.totalSlideCount} 
+                                    activeSlideIndex={currentOnboardingViewRef+1}    
+                                /> 
+                            */}
+                        </div>
                     </div>
-                    <div className="right-button-slot-wrapper">
-                        <div className="right-button-slot w-fit h-fit flex flex-row items-center justify-center gap-4">
+                    <div className="button-slots-wrapper mt-4 flex flex-row items-center justify-between">
+                        <div className="left-button-slot w-fit h-fit flex flex-row items-center justify-center">
                             <button
-                                className="text-white font-medium px-3 py-1.5 rounded-sm bg-white bg-opacity-40 hover:bg-opacity-50 uppercase" 
-                                onClick={() => {
-                                    setCurrentOnboardingView(currentOnboardingViewRef - 1);
-                                }}
-                                style={{
-                                    display: previousButtonVisibilityRef
-                                }}
+                                className="text-purple-500 font-medium px-3 py-1.5 rounded-sm bg-purple-700 bg-opacity-40 hover:bg-opacity-50 uppercase bg-cover" 
+                                onClick={() => setOnboarding(false)}
                             >
-                                Previous
+                                Skip
                             </button>
-                            <button
-                                className="text-white font-medium px-3 py-1.5 rounded-sm bg-purple-600 hover:bg-purple-700 uppercase"
-                                onClick={() => {
-                                    if (currentOnboardingViewRef < contentForOnboardingRef.totalSlideCount) {
-                                        setCurrentOnboardingView(currentOnboardingViewRef + 1);
-                                    }
-                                    if (nextButtonContentRef.toLowerCase() !== "next") {
-                                        setOnboarding(false);
-                                    }
-                                }}
-                            >
-                                {nextButtonContentRef}
-                            </button>
+                        </div>
+                        <div className="right-button-slot-wrapper">
+                            <div className="right-button-slot w-fit h-fit flex flex-row items-center justify-center gap-4">
+                                <button
+                                    className="text-white font-medium px-3 py-1.5 rounded-sm bg-white bg-opacity-40 hover:bg-opacity-50 uppercase" 
+                                    onClick={() => {
+                                        setCurrentOnboardingView(currentOnboardingViewRef - 1);
+                                    }}
+                                    style={{
+                                        display: previousButtonVisibilityRef
+                                    }}
+                                >
+                                    Previous
+                                </button>
+                                <button
+                                    className="text-white font-medium px-3 py-1.5 rounded-sm bg-purple-600 hover:bg-purple-700 uppercase"
+                                    onClick={() => {
+                                        if (currentOnboardingViewRef < contentForOnboardingRef.totalSlideCount) {
+                                            setCurrentOnboardingView(currentOnboardingViewRef + 1);
+                                        }
+                                        if (nextButtonContentRef.toLowerCase() !== "next") {
+                                            setOnboarding(false);
+                                        }
+                                    }}
+                                >
+                                    {nextButtonContentRef}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -171,41 +207,41 @@ function manageOnboardingContent_onSlide(currentState=0) {
 // showSlideCount is a sub-component here
 // getting used to render the active-slide navigation feature
 
-function ShowSlideNavigation({activeSlideIndex=0, totalSlideCount}) {
-    let dotsContainer = [];
-    useEffect(() => {
-        for (let dotCount = 0; dotCount <= totalSlideCount; dotCount++) {
-            dotsContainer.push(
-                dotCount === activeSlideIndex
-                    ? <React.Fragment>
-                        <div className="p-1 w-1 h-1 rounded-full bg-white" />
-                      </React.Fragment>
-                    : <React.Fragment>
-                        <div className="p-1 w-1 h-1 rounded-full bg-white bg-opacity-30" />
-                      </React.Fragment>
-            )
-        }
-    });
+// function ShowSlideNavigation({activeSlideIndex=0, totalSlideCount}) {
+//     let dotsContainer = [];
+//     useEffect(() => {
+//         for (let dotCount = 0; dotCount <= totalSlideCount; dotCount++) {
+//             dotsContainer.push(
+//                 dotCount === activeSlideIndex
+//                     ? <React.Fragment>
+//                         <div className="p-1 w-1 h-1 rounded-full bg-white" />
+//                       </React.Fragment>
+//                     : <React.Fragment>
+//                         <div className="p-1 w-1 h-1 rounded-full bg-white bg-opacity-30" />
+//                       </React.Fragment>
+//             )
+//         }
+//     });
     
-    if (!totalSlideCount) return <React.Fragment></React.Fragment>;
+//     if (!totalSlideCount) return <React.Fragment></React.Fragment>;
 
-    if (!activeSlideIndex) {
-        return (
-            <React.Fragment>
-                <div className="navigation-dots-component-wrapper w-fit h-fit flex flex-row items-center justify-between mx-auto gap-2 mt-2">
-                    {dotsContainer.map((dot, dotIndex) => {
-                        return (
-                            <React.Fragment key={dotIndex}>{dot}</React.Fragment>
-                        )
-                    })}
-                </div>
-            </React.Fragment>
-        )
-    } else {
-        return (
-            <React.Fragment>
+//     if (!activeSlideIndex) {
+//         return (
+//             <React.Fragment>
+//                 <div className="navigation-dots-component-wrapper w-fit h-fit flex flex-row items-center justify-between mx-auto gap-2 mt-2">
+//                     {dotsContainer.map((dot, dotIndex) => {
+//                         return (
+//                             <React.Fragment key={dotIndex}>{dot}</React.Fragment>
+//                         )
+//                     })}
+//                 </div>
+//             </React.Fragment>
+//         )
+//     } else {
+//         return (
+//             <React.Fragment>
 
-            </React.Fragment>
-        )
-    }
-}
+//             </React.Fragment>
+//         )
+//     }
+// }
