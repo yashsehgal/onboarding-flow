2ea8ce7e6ec826ab18877065c2c4029d7175dcaa
Yash Sehgal <yashsehgal.work@gmail.com>
2022-07-02 15:47:57 +0000

    chore: react-responsive-carousel onboarding component added

---
+++ src/ReactResponsiveCarouselOnboarding.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ReactResponsiveCarouselOnboarding() {
+    return (
+        <React.Fragment>
+            <h1 className="leading-snug text-sm font-normal text-purple-500 text-opacity-50 w-[32ch]">
+                React Responsive Carousel Component 
+                will be getting rendered here
+            </h1>
+        </React.Fragment>
+    )
+}
